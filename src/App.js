5ae2780e555d6cfe12b201ce5b9c277cdfcc8576
Khushboo pandit <khushboo17@navgurukul.org>
2020-08-10 12:06:26 +0530

    push code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    books: [],
+    bookName: "",
+    bookDescription: "",
+  };
+  saveInList = () => {
+    let obj = {},
+      lis = this.state.books;
+    obj.name = this.state.bookName;
+    obj.description = this.state.bookDescription;
+    lis.push(obj);
+    this.setState({ bookName: "", bookDescription: "", books: lis });
+  };
+  render() {
+    console.log(this.state.books);
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={this.state.bookName}
+          onChange={(e) => this.setState({ bookName: e.target.value })}
+        ></input>
+        <input
+          type="text"
+          value={this.state.bookDescription}
+          onChange={(e) => this.setState({ bookDescription: e.target.value })}
+        ></input>
+        <button onClick={this.saveInList}>Submit</button>
+
+        {this.state.books.map((book, index) => (
+          <div className="card">
+            <h4>{book.name}</h4>
+            <p>{book.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
